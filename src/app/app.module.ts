4197d001f17d3046e73f174fd0d55198db2529d8
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-25 11:26:05 -0500

    Add SvgWrapper component at app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SvgWrapperComponent } from './components/svg-wrapper/svg-wrapper.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { IconComponent } from './components/icon/icon.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
+    SvgWrapperComponent,
     BrandComponent,
     IconComponent
   ],
