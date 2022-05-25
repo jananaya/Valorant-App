38b4b0997b0c2361172d7e217271c51b7caeb549
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-25 17:05:58 -0500

    Update app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,33 @@ import { AppComponent } from './app.component';
 import { SvgWrapperComponent } from './components/svg-wrapper/svg-wrapper.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { IconComponent } from './components/icon/icon.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { HeaderComponent } from './components/header/header.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [
     AppComponent,
     SvgWrapperComponent,
     BrandComponent,
-    IconComponent
+    IconComponent,
+    HeaderComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
