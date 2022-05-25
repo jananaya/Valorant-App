8af54cbda02ed798b8d7adc470efdcd6ac0c55ca
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-25 16:49:13 -0500

    Add color as property at brand component

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+type Color = 'white' | 'black';
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
 })
 export class BrandComponent {
+  @Input() color: Color = 'black';
 }
