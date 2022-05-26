073db8be1687ba0fa465cf9006ac72dbda5450b3
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-26 15:03:50 -0500

    Create Weapon type

---
+++ src/types/Weapon.ts
@@ -0,0 +1,13 @@
+type Weapon = {
+    id: number;
+    name: string;
+    category: string;
+    specs: string[];
+    imagePath: string;
+    brief: string;
+    rateOfFire: number;
+    damage: number;
+    rating: number;
+}
+
+export default Weapon;
