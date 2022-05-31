6085ff50cdb699f5d3ad12ef0ef5f4b653ea7ff6
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-30 19:22:42 -0500

    Rewrite weapon card component

---
+++ src/app/components/card/weapon-card/weapon-card.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input} from '@angular/core';
+import Weapon from 'src/types/Weapon';
+
+@Component({
+  selector: 'app-weapon-card',
+  templateUrl: './weapon-card.component.html',
+  styleUrls: [
+    './weapon-card.component.css',
+    '../card.component.css'
+  ]
+})
+export class WeaponCardComponent {
+
+  readonly CSS_CLASS_TO_HIDE = 'hidden';
+  @Input() data: Weapon = {
+    id: 0,
+    name: '',
+    category: '',
+    specs: [],
+    imagePath: '',
+    brief: '',
+    rateOfFire: 0,
+    damage: 0,
+    rating: 0
+  };
+
+  isMouseOver: boolean = false;
+  frontDisplay = '';
+  backDisplay = this.CSS_CLASS_TO_HIDE;
+
+  changeDisplayValue() {
+    this.isMouseOver = !this.isMouseOver;
+
+    if (this.isMouseOver) {
+      this.frontDisplay = this.CSS_CLASS_TO_HIDE;
+      this.backDisplay = '';
+    } else {
+      this.frontDisplay = '';
+      this.backDisplay = this.CSS_CLASS_TO_HIDE;
+    }
+  }
+}
