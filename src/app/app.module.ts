3d612480ecc3304ac5941b1481d386a683a1422e
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-30 19:34:37 -0500

    Update app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { BrandComponent } from './components/brand/brand.component';
 import { IconComponent } from './components/icon/icon.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardWrapperComponent } from './components/card-wrapper/card-wrapper.component';
+import { CardHeaderComponent } from './components/card/card-header/card-header.component';
+import { CardComponent } from './components/card/card.component';
+import { WeaponCardComponent } from './components/card/weapon-card/weapon-card.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -15,8 +19,6 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { CardWrapperComponent } from './components/card-wrapper/card-wrapper.component';
-import { WeaponCardComponent } from './components/weapon-card/weapon-card.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,8 @@ import { WeaponCardComponent } from './components/weapon-card/weapon-card.compon
     HeaderComponent,
     SearchInputComponent,
     CardWrapperComponent,
+    CardHeaderComponent,
+    CardComponent,
     WeaponCardComponent
   ],
   imports: [
