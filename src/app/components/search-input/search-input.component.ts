b497956420f76ed1ac41ef811b64d325ce077619
Jan Carlos Anaya Jiménez <jancarlosanayajimenez@gmail.com>
2022-06-21 17:06:45 -0500

    Feature to search weapon by name (#5)
    * Add functionality to search input component
    
    * Modify search input template

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { WeaponsService } from 'src/app/services/weapons.service';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.css']
 })
-export class SearchInputComponent { }
+export class SearchInputComponent {
+  constructor(private weaponsService: WeaponsService) {}
+
+  formatName(name: string): string {
+    let str = name.toLowerCase();
+
+    return str.replace(str.charAt(0), str.charAt(0).toUpperCase());
+  }
+
+  search(name: string) {
+    this.weaponsService.getByName(this.formatName(name)).subscribe((data) => {
+      this.weaponsService.weaponEmitter.emit(data);
+    });
+  }
+}
